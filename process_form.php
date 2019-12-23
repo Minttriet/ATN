2f842773355e295d593bed0013d3c18b47c930ee
Minttriet <46739371+Minttriet@users.noreply.github.com>
2019-12-23 12:36:56 +0700

    sd

---
+++ process_form.php
@@ -0,0 +1,67 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$HOST = "ec2-174-129-24-148.compute-1.amazonaws.com";
+$PORT = "5432";
+$DBNAME = "d1g74t0e6s49db";
+$USER = "tvjtxkjxjvahmk";
+$PASSWORD = "1fa623a191d164af5a074cfea1a65aa3a787ce8ded1c6c9bc4940ddccab0671f";
+
+//$link = pg_connect("host=$HOST dbname=$DBNAME user=$USER password=$PASSWORD sslmode=require");
+ $link = pg_connect("dbname=d1g74t0e6s49db host=ec2-174-129-24-148.compute-1.amazonaws.com port=5432 user=
+tvjtxkjxjvahmk password=1fa623a191d164af5a074cfea1a65aa3a787ce8ded1c6c9bc4940ddccab0671f sslmode=require");
+//$link = pg_connect(getenv("DATABASE_URL"));
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. ");
+}
+ else {echo "Connected";}
+
+// Escape user inputs for security
+
+$id = pg_escape_string ($link, $_REQUEST['id']);
+$name = pg_escape_string ($link, $_REQUEST['name']);
+$cat = pg_escape_string ($link, $_REQUEST['cat']);
+$date = pg_escape_string ($link, $_REQUEST['date']);
+$price = pg_escape_string ($link, $_REQUEST['price']);
+$description = pg_escape_string ($link, $_REQUEST['desc']);
+
+echo $id;
+echo "";
+echo $name;
+echo "";
+echo $cat;
+echo "";
+echo $date;
+echo "";
+echo $price;
+echo "";
+echo $desc;
+echo "";
+
+// Attempt insert query execution
+//$sql = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) VALUES ('$id', '$name', '$cat','$date','$price','abc')";
+//echo $sql;
+
+//$sql2 = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) VALUES ('02', 'Me', 'CatX','2019-12-20',11,'abc')";
+
+$sql3 = 'INSERT INTO public."Product" (
+"Date", "Id", "Product_Name", "Catergory", "Descriptions", "Price") VALUES ('."
+'2019-12-20'::date, '121210'::character varying(20), 'my product XYZ'::character varying(100), 'kit'::character varying(40), 'my product xyz'::character varying(200), '12'::integer)".
+ 'returning "Id"';
+echo $sql3;
+
+$result = pg_query($link, $sql3);
+echo $result;
+
+if($result){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . pg_error($link);
+}
+
+// Close connection
+
+pg_close($link);
+?>
